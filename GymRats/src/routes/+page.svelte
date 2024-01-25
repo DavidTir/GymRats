736e7aff666fed2fef752b36ca616b6622579adf
DavidTir <mt221083@fhstp.ac.at>
2024-01-25 14:39:08 +0100

    Seiten Layout

--- GymRats/src/routes/+page.svelte
+++ GymRats/src/routes/+page.svelte
@@ -1,21 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Header from '../components/header.svelte';
+    import Body from '../components/body.svelte';
+</script>
 
-<div style="width: 100%; height: 100%; background: white; justify-content: center; align-items: center; display: inline-flex">
-    <div style="width: 1920px; height: 1080px; position: relative">
-        <img style="width: 1920px; height: 876px; left: 0px; top: 204px; position: absolute; mix-blend-mode: luminosity" src="https://via.placeholder.com/1920x876" />
-        <div style="width: 1920px; height: 204px; left: 0px; top: 0px; position: absolute">
-            <div style="width: 1920px; height: 204px; left: 0px; top: 0px; position: absolute; background: #36393B"></div>
-            <img style="width: 424px; height: 204px; left: 748px; top: 0px; position: absolute" src="https://via.placeholder.com/424x204" />
-            <div style="width: 185px; height: 44px; left: 871px; top: 146px; position: absolute; color: white; font-size: 36px; font-family: Jost; font-weight: 800; word-wrap: break-word">GYMRATS<br/></div>
-            <div style="left: 323px; top: 84px; position: absolute; color: #F18F01; font-size: 24px; font-family: Jost; font-weight: 500; text-decoration: underline; word-wrap: break-word">Trainingspläne</div>
-            <div style="left: 580px; top: 84px; position: absolute; color: white; font-size: 24px; font-family: Jost; font-weight: 500; word-wrap: break-word">Trainingsorte</div>
-            <div style="left: 1203px; top: 84px; position: absolute; color: white; font-size: 24px; font-family: Jost; font-weight: 500; word-wrap: break-word">Sozial</div>
-            <div style="left: 1419px; top: 84px; position: absolute; color: white; font-size: 24px; font-family: Jost; font-weight: 500; word-wrap: break-word">Mein Profil</div>
-            <div style="left: 1782px; top: 123px; position: absolute; color: white; font-size: 24px; font-family: Jost; font-weight: 500; word-wrap: break-word">Login</div>
-            <div style="width: 78px; height: 78px; left: 1772px; top: 45px; position: absolute; background: #878C8F; border-radius: 9999px"></div>
-            <img style="width: 61px; height: 61px; left: 1780px; top: 53px; position: absolute" src="https://via.placeholder.com/61x61" />
-        </div>
-        <div style="width: 1538px; height: 849px; left: 195px; top: 231px; position: absolute; background: rgba(135, 140, 143, 0.60)"></div>
-    </div>
-</div>
+<Header />
+<Body />
