feb03c1368bffdb8fe64261a451be4415b44704c
Mateusz <mt221026@fhstp.ac.at>
2024-01-25 10:31:50 +0100

    Hochladen der Bilder und Grafiken

--- GymRats/src/routes/+page.svelte
+++ GymRats/src/routes/+page.svelte
@@ -1,2 +1,17 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div style="width: 100%; height: 100%; position: relative">
+    <img style="width: 1942px; height: 1110px; left: 0px; top: 152px; position: absolute; mix-blend-mode: luminosity" src="https://via.placeholder.com/1942x1110" />
+    <div style="width: 1920px; height: 204px; left: 0px; top: 68px; position: absolute; background: #36393B"></div>
+    <img style="width: 424px; height: 351px; left: 748px; top: 0px; position: absolute" src="https://via.placeholder.com/424x351" />
+    <div style="width: 185px; height: 44px; left: 871px; top: 214px; position: absolute; color: #F18F01; font-size: 36px; font-family: Jost; font-weight: 800; text-decoration: underline; word-wrap: break-word">GYMRATS<br/></div>
+    <div style="left: 323px; top: 152px; position: absolute; color: white; font-size: 24px; font-family: Jost; font-weight: 500; word-wrap: break-word">Trainingspläne</div>
+    <div style="left: 580px; top: 152px; position: absolute; color: white; font-size: 24px; font-family: Jost; font-weight: 500; word-wrap: break-word">Trainingsorte</div>
+    <div style="left: 1203px; top: 152px; position: absolute; color: white; font-size: 24px; font-family: Jost; font-weight: 500; word-wrap: break-word">Sozial</div>
+    <div style="left: 1419px; top: 152px; position: absolute; color: white; font-size: 24px; font-family: Jost; font-weight: 500; word-wrap: break-word">Mein Profil</div>
+    <div style="left: 1782px; top: 191px; position: absolute; color: white; font-size: 24px; font-family: Jost; font-weight: 500; word-wrap: break-word">Login</div>
+    <div style="width: 78px; height: 78px; left: 1772px; top: 113px; position: absolute; background: #878C8F; border-radius: 9999px"></div>
+    <img style="width: 61px; height: 61px; left: 1780px; top: 121px; position: absolute" src="https://via.placeholder.com/61x61" />
+    <div style="width: 1538px; height: 860px; left: 195px; top: 299px; position: absolute; background: rgba(135, 140, 143, 0.60)"></div>
+</div>
